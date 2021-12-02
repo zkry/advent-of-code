f691ce39576cfbc9cee18eb8ed97df10914ae4fe
Zachary Romero <zacromero@posteo.net>
2021-12-02 05:58:35 -0700

    Add day 2s solution

--- aoc21.el
+++ aoc21.el
@@ -37,10 +37,15 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun aoc21--puzzle-file-name-for-day (d)
-  "Return the name of puzzle file for a given days puzzle."
+  "Return the name of puzzle file for a given day's D puzzle."
   (format "puzzle%d.txt" d))
 
+(defun aoc21--read-file-for-day (d)
+  "Return the contents of a file for a given day's D puzzle."
+  (f-read (aoc21--puzzle-file-name-for-day d)))
+
 (defun aoc21--read-number-list (fn)
+  "Read a list of numbers is integers from file named FN."
   (let* ((f-contents (f-read fn))
          (lines (split-string f-contents)))
     (seq-map (lambda (l) (read l)) lines)))
@@ -50,6 +55,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun aoc21-day-1-1 ()
+  "Count how many times the values increase."
   (let* ((fn (aoc21--puzzle-file-name-for-day 1))
          (data (aoc21--read-number-list fn))
          (inc-ct 0))
@@ -62,6 +68,7 @@
     inc-ct))
 
 (defun aoc21-day-1-2 ()
+  "Count how many times the sum of 3 consecutive values increase."
   (let* ((fn (aoc21--puzzle-file-name-for-day 1))
          (data (aoc21--read-number-list fn))
          (inc-ct 0)
@@ -73,6 +80,48 @@
               sliding-window
               (cdr sliding-window))
     inc-ct))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Day 2 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun aoc21--day-2-read-commands ()
+  "Read a list of 'forward 1' style commands."
+  (let* ((f-contents (aoc21--read-file-for-day 2))
+         (lines (seq-filter (lambda (s) (not (string-blank-p s)))
+                            (split-string f-contents "\n"))))
+    (seq-map (lambda (l)
+               (let ((parts (split-string l)))
+                 (setcar (cdr parts) (read (cadr parts)))
+                 parts))
+             lines)))
+
+(defun aoc21--day-2-1 ()
+  "Calculate the product of horizontal and depth of the submarine."
+  (let* ((horiz 0)
+         (depth 0)
+         (cmds (aoc21--day-2-read-commands)))
+    (dolist (cmd cmds)
+      (pcase cmd
+        (`("forward" ,amount) (setq horiz (+ horiz amount)))
+        (`("up" ,amount)      (setq depth (- depth amount)))
+        (`("down" ,amount)    (setq depth (+ depth amount)))))
+    (* horiz depth)))
+
+(defun aoc21--day-2-2 ()
+  "Calculate the product of horizontal and depth of the submarine using aim."
+  (let* ((aim 0)
+         (horiz 0)
+         (depth 0)
+         (cmds (aoc21--day-2-read-commands)))
+    (dolist (cmd cmds)
+      (pcase cmd
+        (`("forward" ,amount)
+         (setq horiz (+ horiz amount))
+         (setq depth (+ depth (* aim amount))))
+        (`("up" ,amount)      (setq aim (- aim amount)))
+        (`("down" ,amount)    (setq aim (+ aim amount)))))
+    (* horiz depth)))
 
 (provide 'aoc21)
 
